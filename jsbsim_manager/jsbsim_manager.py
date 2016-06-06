5a41586312d7631932abf3354b73d70c312df5a1
Nathan Bergey <nathan.bergey@gmail.com>
2016-06-06 15:12:28 -0600

    Case: Manage initial conditions

--- jsbsim_manager/jsbsim_manager.py
+++ jsbsim_manager/jsbsim_manager.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as minidom
 
 
 class RunManager(object):
@@ -35,3 +37,48 @@ class Case(object):
 
     def __init__(self):
         pass
+
+    def _append_unit_element(self, parent, element, units, value):
+        e = ET.SubElement(parent, element)
+        e.attrib['unit'] = units
+        e.text = "%f" % value
+
+    def initial_conditions(self,
+                           ubody=0,
+                           vbody=0,
+                           wbody=0,
+                           phi=0,
+                           theta=0,
+                           psi=0,
+                           altitude=0,
+                           latitude=0,
+                           longitude=0,
+                           elevation=0):
+        """Set the initial conditions for the simulation.
+        """
+
+        # initial conditions document:
+        init_doc = ET.Element('initialize')
+        init_doc.attrib['name'] = "Initial Conditions"
+
+        # NED velocity vector:
+        self._append_unit_element(init_doc, 'ubody', 'M/SEC', ubody)
+        self._append_unit_element(init_doc, 'vbody', 'M/SEC', vbody)
+        self._append_unit_element(init_doc, 'wbody', 'M/SEC', wbody)
+
+        # Rotation:
+        self._append_unit_element(init_doc, 'phi', 'DEG', phi)
+        self._append_unit_element(init_doc, 'theta', 'DEG', theta)
+        self._append_unit_element(init_doc, 'psi', 'DEG', psi)
+
+        # Altitude AGL:
+        self._append_unit_element(init_doc, 'altitude', 'M', altitude)
+
+        # Earth position:
+        self._append_unit_element(init_doc, 'latitude', 'DEG', latitude)
+        self._append_unit_element(init_doc, 'longitude', 'DEG', longitude)
+        self._append_unit_element(init_doc, 'elevation', 'M', elevation)
+
+        # Pretty print XML Document
+        xmldoc = minidom.parseString(ET.tostring(init_doc, encoding="UTF-8"))
+        return xmldoc.toprettyxml(indent="  ")
--- tests/test_jsbsim_manager.py
+++ tests/test_jsbsim_manager.py
@@ -15,7 +15,6 @@ import unittest
 from jsbsim_manager import jsbsim_manager
 
 
-
 class TestJsbsim_manager(unittest.TestCase):
 
     def setUp(self):
@@ -27,8 +26,52 @@ class TestJsbsim_manager(unittest.TestCase):
     def test_smoke(self):
         # Really simple smoke test
         case = jsbsim_manager.Case()
-        runer = jsbsim_manager.RunManager(case)
+        runner = jsbsim_manager.RunManager(case)
+        self.assertEqual(type(runner), jsbsim_manager.RunManager)
 
+    def test_init_default(self):
+        case = jsbsim_manager.Case()
+
+        # Pure default values
+        init = case.initial_conditions()
+
+        self.maxDiff = 1500
+        self.assertEqual(init, """<?xml version="1.0" ?>
+<initialize name="Initial Conditions">
+  <ubody unit="M/SEC">0.000000</ubody>
+  <vbody unit="M/SEC">0.000000</vbody>
+  <wbody unit="M/SEC">0.000000</wbody>
+  <phi unit="DEG">0.000000</phi>
+  <theta unit="DEG">0.000000</theta>
+  <psi unit="DEG">0.000000</psi>
+  <altitude unit="M">0.000000</altitude>
+  <latitude unit="DEG">0.000000</latitude>
+  <longitude unit="DEG">0.000000</longitude>
+  <elevation unit="M">0.000000</elevation>
+</initialize>
+""")
+
+    def test_init_override(self):
+        case = jsbsim_manager.Case()
+
+        # Pure default values
+        init = case.initial_conditions(vbody=2.5, latitude=45.0)
+
+        self.maxDiff = 1500
+        self.assertEqual(init, """<?xml version="1.0" ?>
+<initialize name="Initial Conditions">
+  <ubody unit="M/SEC">0.000000</ubody>
+  <vbody unit="M/SEC">2.500000</vbody>
+  <wbody unit="M/SEC">0.000000</wbody>
+  <phi unit="DEG">0.000000</phi>
+  <theta unit="DEG">0.000000</theta>
+  <psi unit="DEG">0.000000</psi>
+  <altitude unit="M">0.000000</altitude>
+  <latitude unit="DEG">45.000000</latitude>
+  <longitude unit="DEG">0.000000</longitude>
+  <elevation unit="M">0.000000</elevation>
+</initialize>
+""")
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
